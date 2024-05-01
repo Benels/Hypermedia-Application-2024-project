150f90bb0fa13eb43f8a22b2eaa7ebe3650e885d
Benels <francesco.benelle@mail.polimi.it>
2024-05-01 11:58:42 +0200

    Initial Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
+
+  modules: ['@nuxtjs/supabase'],// ['@pinia/nuxt'],
+  /*pinia: {
+    autoImports: [
+        'defineStore',
+    ],
+  },*/
   devtools: { enabled: true }
 })
