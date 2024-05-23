9c8e877dee970c97a8866b2d9aec824cfdaf10e7
Benels <francesco.benelle@mail.polimi.it>
2024-05-23 15:10:50 +0200

    Added projects detail page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -4,51 +4,74 @@
   </Head>
 
   <main class="outerContainer">
-    <p>{{ activity.name }}</p>
-    <p>{{ activity.description }}</p>
+    <h1>{{ a.name }}</h1>
+    <h2>Summary of the project</h2>
+    <p>{{summary}}</p>
 
+    <h2>Description of the project</h2>
+    <div v-for="(part, index) in descriptionParts" :key="index" class="description">
+      <p>{{ part }}</p>
+    </div>
+
+    <h2>Leader of the project</h2>
+    <div class="person-card">
+      <router-link :to="'/our_women/' + a.leader">
+        <img class="person-card-img" :src="`https://qpznxdvtbsibmwyurkfl.supabase.co/storage/v1/object/public/person_image/${person.name}-${person.surname}.jpg`" :alt="`Missing Image`" />
+        <h1 class="name_surname">{{ person.name }} {{ person.surname }}</h1>
+      </router-link>
+    </div>
   </main>
 </template>
 
 <script setup>
-
   const route = useRoute()
-  const project = await $fetch('/api/activities/' + route.params.id) //get the specific person from their id
+  const activityId = route.params.id;
+  const {data: project} = await useFetch('/api/activities/' + activityId);
+  console.log(project[0]);
 
-  const leader = await $fetch('/api/our_women/' + project.leader)
+  const a = JSON.parse(JSON.stringify(project.value))[0];
+  console.log(a.leader);
 
-  const descriptionParts = person.description.split('<br>').map(part => part.trim());
+  const lead = a.leader;
+  const person = await $fetch('/api/our_women/' + lead)
 
-
-
+  const descriptionParts = a.description.split('<br>').map(part => part.trim());
+  const summary = descriptionParts[0]
 </script>
 
 <style scoped>
-.outerContainer {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+
+h1{  font-size: 1.5rem;
+  color: #d62828;
+  font-weight: bold;
 }
 
-/* GRID
-.peopleContainer {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  column-gap: 128px;
-  row-gap: 32px;
-}*/
+h2{  font-size: 1.2rem;
+   color: #d62828;
+ }
 
-.peopleContainer {
-  /* gapSize * 2 + personCardWidth * 3 */
-  max-width: calc(128px * 2 + 240px * 3);
+.person-card {
   display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  gap: 128px;
-  row-gap: 32px;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  max-width: 40rem;
+  margin: 2rem auto;
+  background-color: #fff;
+}
+
+.person-card-img {
+  width: 100%;
+  max-width: 20rem;
+  border-radius: 10%;
+  margin-bottom: 1rem;
+}
+
+.name_surname {
+  text-align: center;
+  margin: 0.5rem 0;
 }
 
 </style>
