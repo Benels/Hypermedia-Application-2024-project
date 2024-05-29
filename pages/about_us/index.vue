de9df60394a5fc72a63ef8a153ae84c03f179f59
Benels <francesco.benelle@mail.polimi.it>
2024-05-29 21:04:49 +0200

    our_womens' dropdown menu's arrow now rotates according to the state of the menu

--- pages/about_us/index.vue
+++ pages/about_us/index.vue
@@ -91,7 +91,7 @@
         </div>
 
         <button v-if="acts.length > 1" type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red bg-opacity-75  dark:bg-gray-800/30 group-hover:bg-red dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-red bg-opacity-75  dark:bg-gray-800/30 group-hover:bg-red dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
             </svg>
